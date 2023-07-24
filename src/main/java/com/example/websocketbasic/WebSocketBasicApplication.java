5d8362dd9a81f3cf596265d8aa4f8ddc664298d9
xotlr333 <xotlr333@naver.com>
2023-07-25 01:39:41 +0900

    Initial commit

---
+++ src/main/java/com/example/websocketbasic/WebSocketBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocketbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketBasicApplication.class, args);
+    }
+
+}
